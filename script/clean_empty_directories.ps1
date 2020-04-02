3ea9bf9f4e0e65855b82cf8bf5e1ba6b36c1caa3
Maximilian Starke <maximilian.starke@medionmail.com>
2020-04-02 21:18:34 +0200

    add script

---
+++ script/clean_empty_directories.ps1
@@ -0,0 +1,4 @@
+﻿ECHO "Clearing all empty subdirectories..."
+Get-ChildItem -recurse | Where {$_.PSIsContainer -and `
+@(Get-ChildItem -Force -Lit $_.Fullname -r | Where {!$_.PSIsContainer}).Length -eq 0} |
+Remove-Item -recurse -Confirm
