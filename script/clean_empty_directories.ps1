d81f0f2186390d2263e03c2c58d44b5287977ac4
Maximilian Starke <maximilian.starke@medionmail.com>
2020-04-02 21:18:34 +0200

    script

---
+++ script/clean_empty_directories.ps1
@@ -0,0 +1,4 @@
+﻿ECHO "This Fussels 'Clear Empty Directories' 1.0"
+Get-ChildItem -recurse | Where {$_.PSIsContainer -and `
+@(Get-ChildItem -Force -Lit $_.Fullname -r | Where {!$_.PSIsContainer}).Length -eq 0} |
+Remove-Item -recurse -Confirm
