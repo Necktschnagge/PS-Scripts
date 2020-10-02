786375eb12d99f91b65c1126251712f166ebd9ab
Maximilian Starke <maximilian.starke@medionmail.com>
2020-10-02 23:20:30 +0200

    add-script

---
+++ script/beep_loop.ps1
@@ -0,0 +1,20 @@
+﻿$minutes_frequency = Read-Host "mintues between two consecutive beeps? (recommended: 10)"
+while (1){
+    Write-Output "BEEEEEEEP"
+    #beep
+    $y= 440.0;
+    for($x=0; $x -lt 25; $x++) { 
+        [console]::beep($y,300);
+        $y = $y * 415.30 / 440;
+        Write-Output $y
+    }
+
+    Write-Output "----->"
+    #wait
+    for($i = $minutes_frequency; -not ($i -eq 0); $i--){
+        Write-Output $i
+        Sleep(60);
+    }
+    Write-Output "====================================="
+
+}
